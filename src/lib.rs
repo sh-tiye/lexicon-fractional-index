ef96517a5a0d0cddd2b68a6355e637cbdd1ad0a2
Liyang <liyang@shore.network>
2021-12-13 17:38:17 +0800

    fix: fix the tail of 0

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@ fn midpoint(a: &str, b: &str) -> String {
     let mut i = 0;
     // TODO how Go made this happen?
     // https://github.com/rocicorp/fracdex/blob/main/fracdex.go#L110-L112
-    for _ in 0..std::cmp::max(a.len(), b.len()) {
+    for _ in 0..a.len() {
       let mut c: char = '0';
       if a.len() > i {
         c = a.chars().nth(i).unwrap()
@@ -116,7 +116,10 @@ fn midpoint(a: &str, b: &str) -> String {
 
   // first digits are consecutive
   if b.len() > 1 {
+    if b.chars().nth(0).unwrap().to_string()!="0"{
     return b[0..1].to_string();
+    }
+    return BASE62_DIGITS.chars().nth(digit_a).unwrap().to_string() + &midpoint("", &b[1..]);
   }
 
   // `b` is empty or has length 1 (a single digit).
